72f83158adf4fa0d8d3949e7b53aeeab763fdbb8
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-12-01 19:45:58 -0300

    hecho parte 2

--- p2/src/Library/Offer.cs
+++ p2/src/Library/Offer.cs
@@ -33,5 +33,16 @@ namespace Ucu.Poo.Defense
         {
             this.items.Remove(item);
         }
+        public string AsText()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Fecha: {EndDate}");
+
+            foreach (OfferItem item in items)
+            {
+                sb.AppendLine(item.AsText());
+            }
+            return sb.ToString();
+        }
     }
 }
--- p2/src/Library/OfferItem.cs
+++ p2/src/Library/OfferItem.cs
@@ -17,5 +17,10 @@ namespace Ucu.Poo.Defense
             this.Quantity = quantity;
             this.Price = price;
         }
+
+        public string AsText()
+        {
+            return ($"{Quantity} de '{Residue.Name}' a ${Price}");
+        }
     }
 }
